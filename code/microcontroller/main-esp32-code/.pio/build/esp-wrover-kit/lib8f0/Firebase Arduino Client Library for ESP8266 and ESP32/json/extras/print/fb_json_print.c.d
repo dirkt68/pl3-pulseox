0cd42fb705a46076b9a06c6d92b901d421e658ed
dirkt68 <dirkt6868@gmail.com>
2023-05-05 15:43:43 -0500

    one more

---
+++ code/microcontroller/main-esp32-code/.pio/build/esp-wrover-kit/lib8f0/Firebase Arduino Client Library for ESP8266 and ESP32/json/extras/print/fb_json_print.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp-wrover-kit/lib8f0/Firebase\ Arduino\ Client\ Library\ for\ ESP8266\ and\ ESP32/json/extras/print/fb_json_print.c.o: \
+ .pio/libdeps/esp-wrover-kit/Firebase\ Arduino\ Client\ Library\ for\ ESP8266\ and\ ESP32/src/json/extras/print/fb_json_print.c \
+ .pio/libdeps/esp-wrover-kit/Firebase\ Arduino\ Client\ Library\ for\ ESP8266\ and\ ESP32/src/json/extras/print/fb_json_print.h
